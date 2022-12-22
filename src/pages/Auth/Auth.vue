1738721cd6c4707788ce641f631480e518b4cb8c
melisand <melisandoval@live.com.ar>
2022-12-22 20:57:12 +0100

    add a log in error component to Auth page

--- src/pages/Auth/Auth.vue
+++ src/pages/Auth/Auth.vue
@@ -1,14 +1,21 @@
 <template>
-  <LogIn v-if="userHasAnAccount" @toggleForm="toggleForm" />
-  <SignUp v-else @toggleForm="toggleForm" />
+  <LogIn
+    v-if="userHasAnAccount && userConfirmedEmail"
+    @toggleForm="toggleForm"
+  />
+  <LogInErrorMsg v-if="userHasAnAccount && !userConfirmedEmail" />
+  <SignUp v-if="!userHasAnAccount" @toggleForm="toggleForm" />
 </template>
 
 <script setup>
 import LogIn from "./components/LogIn.vue";
 import SignUp from "./components/SignUp.vue";
+import LogInErrorMsg from "./components/LogInErrorMsg.vue";
+
 import { ref } from "vue";
 
 const userHasAnAccount = ref(true);
+const userConfirmedEmail = ref(true);
 
 function toggleForm() {
   userHasAnAccount.value = !userHasAnAccount.value;
---
+++ src/pages/Auth/components/LogInErrorMsg.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>
+      Looks like you haven't confirmed your email account, please check your
+      email inbox and confirm your account.
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
