56f1f8a39fdf6262916095bccb352407b3781ab2
melisand <melisandoval@live.com.ar>
2022-12-22 19:39:03 +0100

    Remove ignored files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../pages/Dashboard.vue";
+import Auth from "../pages/Auth.vue";
 
-const routes = [];
+const routes = [
+  { path: "/", component: Dashboard },
+  { path: "/auth", component: Auth },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_ENV),
