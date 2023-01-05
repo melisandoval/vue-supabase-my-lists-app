edaf62c9a938f210d2b842d813c1423c4a80d17e
melisand <melisandoval@live.com.ar>
2023-01-05 22:45:19 +0100

    remove input name field from log in form

--- src/pages/LogIn/components/LogInForm.vue
+++ src/pages/LogIn/components/LogInForm.vue
@@ -2,18 +2,6 @@
   <main class="flex items-center justify-center h-screen">
     <form class="w-full max-w-xs flex-col">
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-          Name *
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
-          type="text"
-          placeholder="e.g. Bob Smith"
-          required
-        />
-      </div>
-      <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
           Email *
         </label>
