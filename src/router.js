3cd309235a023ed5334ea388ee2515a8471a9343
melisand <melisandoval@live.com.ar>
2023-01-10 21:31:44 +0100

    change name of Home/Dashboard component

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "./pages/Dashboard/Dashboard.vue";
+import HomeDashboard from "./pages/HomeDashboard/HomeDashboard.vue";
 import LogIn from "./pages/LogIn/LogIn.vue";
 import Register from "./pages/Register/Register.vue";
 import RegistrationResponse from "./pages/RegistrationResponse/RegistrationResponse.vue";
 
 const routes = [
-  { path: "/", name: "Dashboard", component: Dashboard },
+  { path: "/", name: "Home Dashboard", component: HomeDashboard },
   { path: "/login", name: "Log in", component: LogIn },
   { path: "/register", name: "Register", component: Register },
   {
