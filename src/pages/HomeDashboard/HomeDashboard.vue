48e480a79554b552a5ad6e700f26a30064558b02
melisand <melisandoval@live.com.ar>
2023-01-17 17:22:58 +0100

    add homepage navbar

--- src/pages/HomeDashboard/HomeDashboard.vue
+++ src/pages/HomeDashboard/HomeDashboard.vue
@@ -1,20 +1,17 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <div class="p-4 bg-white rounded-lg shadow-md">
-      <p class="text-center text-lg font-medium">Home/Dashboard</p>
-      <button
-        @click="logOut"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Log out
-      </button>
-    </div>
-  </div>
+  <button
+    @click="logOut"
+    class="bg-blue-500 hover:bg-blue-700 absolute top-0 right-0 text-white font-bold py-2 px-4 my-6 mx-8 rounded focus:outline-none focus:shadow-outline"
+  >
+    Log out
+  </button>
+  <Navbar />
 </template>
 
 <script setup>
 import { useUserStore } from "../../piniaStores/userStore.js";
 import { useRouter } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 
 const userStore = useUserStore();
 const router = useRouter();
---
+++ src/pages/HomeDashboard/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="h-screen">
+    <nav
+      class="bg-gray-200 min-w-min max-w-xl px-8 flex flex-col absolute inset-y-0 left-0"
+    >
+      <div
+        class="text-3xl text-blue-500 py-16 px-6 block font-medium leading-relaxed"
+      >
+        <h1>
+          Welcome, <br />
+          <span class="text-gray-900">{{
+            userStore.user.user_metadata.name
+          }}</span>
+        </h1>
+      </div>
+
+      <div class="text-lg py-3 px-6 block">
+        <h2>Your lists:</h2>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "../../../piniaStores/userStore.js";
+const userStore = useUserStore();
+</script>
+
+<style lang="scss" scoped></style>
