6a05c19ee11e9f845192c4b57ccdd42e06067a7b
melisand <melisandoval@live.com.ar>
2023-01-24 20:35:26 +0100

    add primary button hover

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -13,8 +13,15 @@ button {
   background-color: #fe8684;
   font-size: 1.4rem;
   padding: 0.4rem 1rem;
+  border: 0.1rem solid #fe8684;
   border-radius: 0.5rem;
   margin: 1rem;
   cursor: pointer;
+}
+
+button:hover {
+  background-color: white;
+  color: #fe8684;
+  border: 0.1rem solid #fe8684;
 }
 </style>
