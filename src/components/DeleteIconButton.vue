0e3714c196ca98a4a8bf6423863966a9853ea041
melisand <melisandoval@live.com.ar>
2023-01-27 22:14:15 +0100

    create icon buttons components

---
+++ src/components/DeleteIconButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="handleClick" :class="class">
+    <svg
+      class="action-icon"
+      width="16px"
+      height="16px"
+      viewBox="0 0 24 24"
+      stroke-width="2.02"
+      fill="#ffffff"
+      xmlns="http://www.w3.org/2000/svg"
+      color="#fe8684"
+    >
+      <path
+        d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"
+        stroke="#fe8684"
+        stroke-width="2.02"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps(["handleClick", "class"]);
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/HomeDashboard/components/Navbar/components/ListTitleButtonDiv.vue
+++ src/pages/HomeDashboard/components/Navbar/components/ListTitleButtonDiv.vue
@@ -4,52 +4,19 @@
       {{ title }}
     </button>
     <div class="title-edit-delete-buttons-container">
-      <button @click="handleEditList" class="action-icon-button">
-        <svg
-          class="action-icon"
-          width="16px"
-          height="16px"
-          stroke-width="2.02"
-          viewBox="0 0 24 24"
-          fill="#ffffff"
-          xmlns="http://www.w3.org/2000/svg"
-          color="#fe8684"
-        >
-          <path
-            d="M13.02 5.828L15.85 3l4.949 4.95-2.829 2.828m-4.95-4.95l-9.606 9.607a1 1 0 00-.293.707v4.536h4.536a1 1 0 00.707-.293l9.606-9.607m-4.95-4.95l4.95 4.95"
-            stroke="#fe8684"
-            stroke-width="2.02"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-        </svg>
-      </button>
-      <button @click="showDeleteListModal" class="action-icon-button">
-        <svg
-          class="action-icon"
-          width="16px"
-          height="16px"
-          viewBox="0 0 24 24"
-          stroke-width="2.02"
-          fill="#ffffff"
-          xmlns="http://www.w3.org/2000/svg"
-          color="#fe8684"
-        >
-          <path
-            d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"
-            stroke="#fe8684"
-            stroke-width="2.02"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-        </svg>
-      </button>
+      <EditIconButton @click="showEditListModal" class="action-icon-button" />
+      <DeleteIconButton
+        @click="showDeleteListModal"
+        class="action-icon-button"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import { useListsStore } from "../../../../../piniaStores/listsStore";
+import EditIconButton from "../../../../../components/EditIconButton.vue";
+import DeleteIconButton from "../../../../../components/DeleteIconButton.vue";
 
 const props = defineProps(["title", "listId"]);
 const emit = defineEmits(["deleteList"]);
@@ -65,7 +32,7 @@ function handleShowList() {
 }
 
 // makes the edit list modal appears:
-function handleEditList() {
+function showEditListModal() {
   listsStore.selectListToEdit({
     listName: props.title,
     listId: props.listId,
