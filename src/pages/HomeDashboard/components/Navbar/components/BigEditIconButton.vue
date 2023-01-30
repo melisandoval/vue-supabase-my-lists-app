e115daa8209d6298291c07d01eafefe3a0b4f6ca
melisand <melisandoval@live.com.ar>
2023-01-30 19:37:31 +0100

    change style of svg

--- src/pages/HomeDashboard/components/Navbar/components/BigEditIconButton.vue
+++ src/pages/HomeDashboard/components/Navbar/components/BigEditIconButton.vue
@@ -3,7 +3,7 @@
     <svg
       width="29px"
       height="29px"
-      stroke-width="2.06"
+      stroke-width="1.60"
       viewBox="0 0 24 24"
       fill="#ffffff"
       xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +12,7 @@
       <path
         d="M13.02 5.828L15.85 3l4.949 4.95-2.829 2.828m-4.95-4.95l-9.606 9.607a1 1 0 00-.293.707v4.536h4.536a1 1 0 00.707-.293l9.606-9.607m-4.95-4.95l4.95 4.95"
         stroke="#fe8684"
-        stroke-width="2.06"
+        stroke-width="1.60"
         stroke-linecap="round"
         stroke-linejoin="round"
       ></path>
