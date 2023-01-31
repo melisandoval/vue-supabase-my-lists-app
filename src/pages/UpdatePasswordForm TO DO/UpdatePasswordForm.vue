53a02ebac7feee75248a12f9bb70fbbacabc8c19
melisand <melisandoval@live.com.ar>
2023-01-31 01:48:02 +0100

    remove unused files

--- src/pages/UpdatePasswordForm TO DO/UpdatePasswordForm.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Update password form</h1>
-  </div>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,30 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeDashboard from "./pages/HomeDashboard/HomeDashboard.vue";
 import Auth from "./pages/Auth/Auth.vue";
-// import LogInPage from "./pages/LogIn/LogInPage.vue";
-// import RegisterPage from "./pages/Register/RegisterPage.vue";
-// import ResetPasswordForm from "./pages/ResetPasswordForm/ResetPasswordForm.vue";
-// import UpdatePasswordForm from "./pages/UpdatePasswordForm/UpdatePasswordForm.vue";
 
 const routes = [
   { path: "/", name: "Home Dashboard", component: HomeDashboard },
-  // { path: "/login", name: "LogIn Page", component: LogInPage },
-  // { path: "/register", name: "Register Page", component: RegisterPage },
-  // {
-  //   path: "/reset-password",
-  //   name: "Reset Password Form",
-  //   component: ResetPasswordForm,
-  // },
-  // {
-  //   path: "/update-password",
-  //   name: "Update Password Form",
-  //   component: UpdatePasswordForm,
-  // },
-  {
-    path: "/auth",
-    name: "Auth",
-    component: Auth,
-  },
+  { path: "/auth", name: "Auth", component: Auth },
 ];
 
 const router = createRouter({
